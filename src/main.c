c42e511d44935f49ed3733bc75c0e7f3289c56f6
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2014-03-29 21:37:41 -0400

    switch to white space

--- src/main.c
+++ src/main.c
@@ -4,101 +4,99 @@
 
 void exit_failure()
 {
-	asm("mov	$1, %eax \n"
-		"mov	$1, %ebx \n"
-		"int	$0x80");
+    asm("mov $1, %eax \n"
+        "mov $1, %ebx \n"
+        "int $0x80");
 }
 
 int read_file(char *filename, char content[], int size)
 {
-	int i;
+    int i;
 
-	FILE *file;
+    FILE *file;
 
-	file = fopen(filename, "r");
-	if(file == NULL)
-	{
-		printf("ERROR: Could not open file %s\n", filename);
-		exit_failure();
-	}
+    file = fopen(filename, "r");
+    if(file == NULL)
+    {
+        printf("ERROR: Could not open file %s\n", filename);
+        exit_failure();
+    }
 
-	for(i = 0; i<size; i++)
-	{
-		content[i] = fgetc(file);
-		if(content[i] == '\n')
-		{
-			content[i] = '\0';
-			i = size;
-		}
-	}
+    for(i = 0; i<size; i++)
+    {
+        content[i] = fgetc(file);
+        if(content[i] == '\n')
+        {
+            content[i] = '\0';
+            i = size;
+        }
+    }
 
-	return 0;
+    return 0;
 }
 
 int string_to_int(char *string)
 {
-	int  i, n;
+    int  i, n;
 
-	n = 0;
+    n = 0;
 
-	for(i = 0; string[i] != '\0'; i++)
-	{
-		n = (n*10)+(string[i]-'0');
-	}
+    for(i = 0; string[i] != '\0'; i++)
+        n = (n*10)+(string[i]-'0');
 
-	return n;
+    return n;
 }
 
 int string_compare(char *string1, char *string2)
 {
-	int i;
+    int i;
 
-	i = 0;
-	while(i >= 0)
-	{
-		if(string1[i] != string2[i])
-			i = -1;
-		else
-			if(string1[i] != '\0')
-				i++;
-			else
-				i = -2;
-	}
+    i = 0;
+    while(i >= 0)
+    {
+        if(string1[i] != string2[i])
+            i = -1;
+        else
+            if(string1[i] != '\0')
+                i++;
+            else
+                i = -2;
+    }
 
-	return i;
+    return i;
 }
 
 int print_info()
 {
-	char content[READ_SIZE];
-	int current;
-	int maximum;
-	int percent;
+    char content[READ_SIZE];
+    int current;
+    int maximum;
+    int percent;
 
-	read_file("/sys/class/power_supply/"ADAPTER"/charge_now" , content, READ_SIZE);
-	current = string_to_int(content);
+    read_file("/sys/class/power_supply/"ADAPTER"/charge_now" , content, READ_SIZE);
+    current = string_to_int(content);
 
-	read_file("/sys/class/power_supply/"ADAPTER"/charge_full", content, READ_SIZE);
-	maximum = string_to_int(content);
+    read_file("/sys/class/power_supply/"ADAPTER"/charge_full", content, READ_SIZE);
+    maximum = string_to_int(content);
 
-	read_file("/sys/class/power_supply/"ADAPTER"/status", content, READ_SIZE);
-	if(string_compare(content, "Charging\0") == -2)
-		printf(P_CHAR P_SEPA);
-	else if(string_compare(content, "Discharging\0") == -2)
-		printf(P_DISC P_SEPA);
-	else
-		printf(P_FULL P_SEPA);
+    read_file("/sys/class/power_supply/"ADAPTER"/status", content, READ_SIZE);
+    if(string_compare(content, "Charging\0") == -2)
+        printf(P_CHAR P_SEPA);
+    else if(string_compare(content, "Discharging\0") == -2)
+        printf(P_DISC P_SEPA);
+    else
+        printf(P_FULL P_SEPA);
 
-	percent = 100*current/maximum;
-	printf("%d%%\n", percent);
+    percent = 100*current/maximum;
+    printf("%d%%\n", percent);
 
-	return 0;
+    return 0;
 }
 
 int main()
 {
-	print_info();
+    print_info();
 
-	return 0;
+    return 0;
 }
 
